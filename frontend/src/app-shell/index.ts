185e82f60387cde58ad1bbef58724ec70c0083f9
bcvolker <slate360ceo@gmail.com>
2025-09-04 22:28:38 -0700

    Fix SimpleAppShell export - this should resolve the deployment issue

--- frontend/src/app-shell/index.ts
+++ frontend/src/app-shell/index.ts
@@ -1,4 +1,5 @@
 // App Shell exports
 export { default as AppShell } from './AppShell';
+export { default as SimpleAppShell } from './SimpleAppShell';
 export { default as Providers } from './Providers';
-export { default as SimpleLayout } from './SimpleLayout';
+export { default as SimpleAppShell } from './SimpleAppShell';`nexport { default as SimpleLayout } from './SimpleLayout';
